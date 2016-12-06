76c8edcb8a16ce10ec44f63ceb1c65d22db66e79
MAI XUAN TRANG <trang@MAIs-MacBook-Pro.local>
2016-12-06 10:11:23 +0900

    Update WebSocketServer

--- server.js
+++ server.js
@@ -61,3 +61,31 @@ var server = http.createServer(function(request, response) {
 });
 server.listen(port);
 console.log('Server running on ' + ip + ':' + port);
+
+var WebSocketServer = require('ws').Server
+var uuid = require('uuid');
+var wss = new WebSocketServer({
+    path: '/gencode',
+    server: server,
+    autoAcceptConnections: false
+});
+var clients = {};
+var dumCounter = 0;
+wss.on('connection', function connection(ws) {
+    ws.on('message', function incoming(message) {
+        console.log('received: %s', message);
+        var obj = JSON.parse(message);
+        if (obj.op == 'hello') {
+            var uuidToken = uuid.v1();
+            clients[uuidToken] = ws;
+            var hello = {
+                op: 'hello',
+                token: uuidToken
+            };
+            ws.send(JSON.stringify(hello), {
+                mask: false
+            });
+        }
+
+    });
+});
