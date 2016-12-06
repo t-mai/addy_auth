28dc04d11556259f5680cb5127ab694b65bc4b3e
MAI XUAN TRANG <trang@MAIs-MacBook-Pro.local>
2016-12-06 10:04:37 +0900

    Fix

--- server.js
+++ server.js
@@ -6,45 +6,58 @@ var os = require('os')
 var port = process.env.PORT || process.env.port || process.env.OPENSHIFT_NODEJS_PORT || 8080;
 var ip = process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 var nodeEnv = process.env.NODE_ENV || 'unknown';
-var server = http.createServer(function (req, res) {
-	var url_parts = url.parse(req.url, true);
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {
+        "Content-Type": "text/plain",
+        "Access-Control-Allow-Origin": "*"
+    });
+    process.on('uncaughtException', function(err) {
+        response.end("Exception");
+    });
 
-	var body = '';
-	req.on('data', function (data) {
-		body += data;
-	});
-	req.on('end', function () {
-		var formattedBody = qs.parse(body);
+    try {
+        if (request.method == "POST") {
+            var url = request.url;
+            if (url == "/auth") {
 
-		res.writeHead(200, {'Content-Type': 'text/plain'});
+                var body = '';
+                request.on('data', function(chunk) {
+                    body += chunk.toString();
+                });
 
-		res.write('This is a node.js echo service\n');
-		res.write('Host: ' + req.headers.host + '\n');
-		res.write('\n');
-		res.write('node.js Production Mode: ' + (nodeEnv == 'production' ? 'yes' : 'no') + '\n');
-		res.write('\n');
-		res.write('HTTP/' + req.httpVersion +'\n');
-		res.write('Request headers:\n');
-		res.write(util.inspect(req.headers, null) + '\n');
-		res.write('Request query:\n');
-		res.write(util.inspect(url_parts.query, null) + '\n');
-		res.write('Request body:\n');
-		res.write(util.inspect(formattedBody, null) + '\n');
-		res.write('\n');
-		res.write('Host: ' + os.hostname() + '\n');
-		res.write('OS Type: ' + os.type() + '\n');
-		res.write('OS Platform: ' + os.platform() + '\n');
-		res.write('OS Arch: ' + os.arch() + '\n');
-		res.write('OS Release: ' + os.release() + '\n');
-		res.write('OS Uptime: ' + os.uptime() + '\n');
-		res.write('OS Free memory: ' + os.freemem() / 1024 / 1024 + 'mb\n');
-		res.write('OS Total memory: ' + os.totalmem() / 1024 / 1024 + 'mb\n');
-		res.write('OS CPU count: ' + os.cpus().length + '\n');
-		res.write('OS CPU model: ' + os.cpus()[0].model + '\n');
-		res.write('OS CPU speed: ' + os.cpus()[0].speed + 'mhz\n');
-		res.end('\n');
+                request.on('end', function() {
+                    var params = JSON.parse(body);
+                    console.log("Recived Params: " + JSON.stringify(params));
+                    var uuId = params.uuid;
+                    var accessToken = params.access_token;
 
-	});
+                    var msg = {
+                        'op': 'authdone',
+                        'accessToken': accessToken
+                    };
+                    if (clients[uuId] != undefined || clients[uuId] != null) {
+                        clients[uuId].send(JSON.stringify(msg), {
+                            mask: false
+                        });
+                        delete clients[uuId];
+                        response.end('{"status":"OK"}');
+
+                    } else {
+                        response.end('{"status":"NOK"}');
+                    }
+                });
+            } else {
+                response.end('{"status":"NOK"}');
+
+            }
+        } else {
+            response.end("NOT Supported");
+        }
+
+    } catch (e) {
+        response.end("Exception");
+
+    }
 });
 server.listen(port);
 console.log('Server running on ' + ip + ':' + port);
